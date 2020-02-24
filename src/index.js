88fcfc2c7bb373f7db307190b4cc49ce51bfe545
kamran kotobi <kotobigit@gmail.com>
2020-02-23 20:09:50 -0600

    if the app fails, it's better if it crashes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import routes from './routes';
 import PORT from './helpers/get-api-port';
 import apiDirectoryMessage from './helpers/api-directory-message';
 
-require('./helpers/uncaught-errors-listener');
 
 // Main app processing starts here
 const app = express();
